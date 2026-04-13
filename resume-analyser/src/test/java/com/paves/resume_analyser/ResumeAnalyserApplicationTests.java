8e36194272f43af059f2640b8b385effc38018dd
Niharika <kandukoori1919@gmail.com>
2026-04-13 23:53:46 +0530

    Initial commit - Full stack AI Resume Analyser (backend + frontend)

---
+++ resume-analyser/src/test/java/com/paves/resume_analyser/ResumeAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paves.resume_analyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
